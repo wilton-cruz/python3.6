1d47943ab3473cf57354eb5f805bd85c07dba2a0
wilton <wiltoncruz5@gmail.com>
2018-01-12 12:53:40 -0500

    reordering format file

--- format.py
+++ format.py
@@ -4,12 +4,13 @@ s = 'Hello, World.'
 x = 10 * 3.25
 y = 200 * 200
 s = 'The value of x is' + repr(x) + ', and y is ' + repr(y) + '...'
-# The repr() of a string adds string quotes and backslashes:
 
-hello = 'Hello, world\n'
-hellos = repr(hello)
-# s = str(1/7)
 # print(str(s))
 # print(repr(s))
+
+# The repr() of a string adds string quotes and backslashes:
+hello = 'Hello, world\n'
 # print(hello) this will output: Hello, world
+
+hellos = repr(hello)
 # print(hellos) this will output: 'Hello, world\n'
